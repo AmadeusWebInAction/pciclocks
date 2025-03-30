99d1b8ab28d70d7702d3e5326b7ff5a5c038d9ef
Subash <precisionclocks@gmail.com>
2025-03-30 22:55:49 +0530

    Few Changes

--- content/make-an-enquiry.md
+++ content/make-an-enquiry.md
@@ -11,6 +11,7 @@ Please tell us the following about you
 And about the clock system required
 
 * Height from ground to tower (this will determing the dial height)
+* Clocks with Hourly Chimes & striking or only Clocks
 * pillar height (this will determine how much distance it can be viewed from)
 * number of faces needed
-* material to use (Mils Steel / Aluminium / Fibre)
+* material to use (Mild Steel / Stainless steel / Aluminium / Fibre)
--- index.php
+++ index.php
@@ -6,19 +6,17 @@ define('SITEPATH', __DIR__);
 include_once '../../awe/core/framework/1-entry.php';
 
 variables([
-	'phone' => '+91-9853860155',
-	'email' => 'precisionclocks@gmail.com',
-	'whatsapp' => '919940226317',
-
 	'sections-have-files' => true,
 	'use-site-static' => true,
 	'not-a-network' => true,
 	'theme' => 'canvas',
 	'sub-theme' => 'flip',
+	'no-page-menu' => true,
 	'social' => [
 //		[ 'type' => 'linkedin', 'url' => 'https://www.linkedin.com/company/pciclocks/', 'name' => 'PCI Clocks' ],
-		[ 'type' => 'linkedin', 'url' => 'https://www.linkedin.com/in/subash-pciclocks/', 'name' => 'Subash' ],
+		[ 'type' => 'linkedin', 'url' => 'https://www.linkedin.com/in/subash-pciclocks/', 'name' => 'Dehury' ],
 //		[ 'type' => 'youtube', 'url' => 'https://www.youtube.com/@pciclocks', 'name' => 'PCI Clocks' ],
+		[ 'type' => 'xx fa-map-location', 'url' => 'https://www.google.co.in/maps/place/Precision+Clocks+%26+Industries/@20.8505093,85.1069155,19z/data=!4m6!3m5!1s0x3a18b1fad1b6d305:0xd7e7d2299c45c248!8m2!3d20.8508711!4d85.10758!16s%2Fg%2F11fkdxlw02?entry=ttu&g_ep=EgoyMDI1MDMxOS4yIKXMDSoJLDEwMjExNDUzSAFQAw%3D%3D', 'name' => 'Workshop' ],
 	]
 ]);
 
